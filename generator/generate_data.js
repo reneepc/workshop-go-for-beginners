f20ccfd0750360ded40c1bf37f5e0221c27c3a9a
reneepc <rcardozo@tractian.com>
2024-07-04 00:42:54 -0300

    feat: add random generator in javascript

---
+++ generator/generate_data.js
@@ -0,0 +1,25 @@
+const faker = require('faker-br');
+const fs = require('fs');
+
+const numRecords = 10000;
+const outputPath = '../data/large_users.csv';
+
+const headers = 'Email,Name,Telephone,CPF,Address\n';
+
+const generateUser = () => {
+    const email = faker.internet.email();
+    const name = faker.name.findName();
+    const telephone = faker.phone.phoneNumber();
+    const cpf = faker.br.cpf();
+    const address = faker.address.streetAddress();
+    return `${email},${name},${telephone},${cpf},${address}\n`;
+};
+
+fs.writeFileSync(outputPath, headers);
+
+for (let i = 0; i < numRecords; i++) {
+    const user = generateUser();
+    fs.appendFileSync(outputPath, user);
+}
+
+console.log(`Generated ${numRecords} records in ${outputPath}`);
