f2b2eccc5d54818211e67852d49419327d4b2c4d
reneepc <rcardozo@tractian.com>
2024-07-04 00:58:48 -0300

    feat: save winners to output file

--- parser.go
+++ parser.go
@@ -21,3 +21,21 @@ func ParseCsvFile(filePath string) ([][]string, error) {
 
 	return records, nil
 }
+
+func SaveWinners(filePath string, winners [][]string) error {
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, winner := range winners {
+		if err := writer.Write(winner); err != nil {
+			return err
+		}
+	}
+	return nil
+}
