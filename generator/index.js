b6963110846e5cd438f86d0663d319dc8615b61a
reneepc <rcardozo@tractian.com>
2024-08-03 21:17:27 -0300

    fix: unfair data filename

--- generator/index.js
+++ generator/index.js
@@ -4,7 +4,7 @@ function runGenerators() {
     try {
         generateUsers(10, 'testdata/users_001.csv', 0);
         generateUsers(10000, 'testdata/users_002.csv', 0);
-        generateUsers(10000, 'testdata/users.csv', 0.4);
+        generateUsers(10000, 'testdata/users_003.csv', 0.4);
     } catch (error) {
         console.error(`Error running generators: ${error}`);
     }
