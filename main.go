e28553638f0aad434923c8a4b25cb767b8ad17f3
reneepc <rcardozo@tractian.com>
2024-08-05 21:16:45 -0300

    feat: send emails to selected users

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
+	"os"
+
+	"github.com/go-gomail/gomail"
 )
 
 func main() {
-	userCsvFile := flag.String("file", "data/large_users.csv", "Path to the user CSV file")
+	userCsvFile := flag.String("file", "testdata/users_001.csv", "Path to the user CSV file")
 	numberOfWinners := flag.Int("winners", 6, "Number of winners to select")
 
 	flag.Parse()
@@ -26,7 +28,46 @@ func main() {
 		log.Fatalf("Error selecting winners: %v", err)
 	}
 
-	for _, winner := range winners {
-		fmt.Printf("Winner: %s\n", winner)
+	if err := SendEmail(winners); err != nil {
+		log.Fatalf("Error sending email to winners")
 	}
+
+	if err := SaveWinners("winners.csv", winners); err != nil {
+		log.Fatalf("Error saving winners to csv")
+	}
+}
+
+func SendEmail(winners [][]string) error {
+	email := os.Getenv("EMAIL")
+	password := os.Getenv("EMAIL_PASSWORD")
+	dialer := gomail.NewDialer("smtp.gmail.com", 587, email, password)
+
+	emails := extractEmails(winners)
+
+	message := gomail.NewMessage()
+	message.SetHeader("Subject", "You are a winner!")
+	message.SetHeader("From", email)
+	message.SetHeader("To", emails...)
+
+	htmlString, err := os.ReadFile("email.html")
+	if err != nil {
+		log.Fatalf("Error reading email template: %v", err)
+	}
+
+	message.SetBody("text/html", string(htmlString))
+
+	if err := dialer.DialAndSend(message); err != nil {
+		log.Fatalf("Error sending email: %v", err)
+	}
+
+	return nil
+}
+
+func extractEmails(winners [][]string) []string {
+	emails := make([]string, 0, len(winners))
+	for _, winner := range winners {
+		emails = append(emails, winner[0])
+	}
+
+	return emails
 }
